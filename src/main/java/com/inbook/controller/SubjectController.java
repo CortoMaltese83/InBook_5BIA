32fde45c873306da0e384630b2369f8fc3a0bfe0
gaetann1 <o.piccagaetano@gmail.com>
2026-03-02 11:59:34 +0100

    Materia Controller

--- src/main/java/com/inbook/controller/SubjectController.java
+++ src/main/java/com/inbook/controller/SubjectController.java
@@ -1,7 +1,18 @@
 package com.inbook.controller;
 
+import com.inbook.service.SubjectService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SubjectController {
+
+    private final SubjectService subjectService;
+    public SubjectController(SubjectService subjectService) {this.subjectService = subjectService;}
+
+    @GetMapping("/load-materia")
+    public String loadmateria() {
+
+        return "";
+    }
 }
