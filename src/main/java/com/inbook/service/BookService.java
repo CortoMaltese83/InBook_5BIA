fca5fc8f0d9e7a17ab5495186a1cbd8a8d811a38
CarmineMario <linkcastellaneta@gmail.com>
2026-03-17 10:43:58 +0100

    Cambiata in BookRepository la List facendola diventare un tipo Optional e aggiunti i metodi addBook e modifyBook. Da completare

--- src/main/java/com/inbook/service/BookService.java
+++ src/main/java/com/inbook/service/BookService.java
@@ -12,7 +12,7 @@ public class BookService {
 
     public BookService(BookRepository bookRepository) { this.bookRepository = bookRepository; }
 
-    public BookRepository addBook(String isbn, String autore, String titolo, int volume, String casaEditrice, double prezzo, boolean daAcquistare, boolean consigliato){
+    public void addBook(String isbn, String autore, String titolo, int volume, String casaEditrice, double prezzo, boolean daAcquistare, boolean consigliato){
 
         Book book = new Book();
 
