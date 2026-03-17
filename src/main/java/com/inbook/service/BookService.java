6d4cd9d410581a99e6af187b7b693531f0b83955
CarmineMario <linkcastellaneta@gmail.com>
2026-03-17 10:41:37 +0100

    Cambiata in BookRepository la List facendola diventare un tipo Optional e aggiunti i metodi addBook e modifyBook. Da completare

--- src/main/java/com/inbook/service/BookService.java
+++ src/main/java/com/inbook/service/BookService.java
@@ -1,20 +1,46 @@
 package com.inbook.service;
 
-import com.inbook.dto.Book;
+
 import com.inbook.repository.BookRepository;
+import com.inbook.repository.entity.Book;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
-    private final BookRepository repo;
+    private final BookRepository bookRepository;
 
-    public BookService(BookRepository repo) { this.repo = repo; }
+    public BookService(BookRepository bookRepository) { this.bookRepository = bookRepository; }
 
-    public BookRepository addBook(String isbn, String autore, String titolo, int volume, String casaEditrice, int prezzo, boolean daAcquistare, boolean consigliato){
+    public void addBook(String isbn, String autore, String titolo, int volume, String casaEditrice, int prezzo, boolean daAcquistare, boolean consigliato){
 
         Book book = new Book();
 
-        return repo.save(book);
+        book.setIsbn(isbn);
+        book.setAutore(autore);
+        book.setTitolo(titolo);
+        book.setVolume(volume);
+        book.setCasaEditrice(casaEditrice);
+        book.setPrezzo(prezzo);
+        book.setDaAcquistare(daAcquistare);
+        book.setConsigliato(consigliato);
+
+        bookRepository.save(book);
     }
+
+    public Book modifyBook(String isbn, String autore, String titolo, int volume, String casaEditrice, int prezzo, boolean daAcquistare, boolean consigliato) {
+        Book book = bookRepository.findByIsbn(isbn).orElseThrow(() -> new RuntimeException("libro non trovato"));
+
+        book.setIsbn(isbn);
+        book.setAutore(autore);
+        book.setTitolo(titolo);
+        book.setVolume(volume);
+        book.setCasaEditrice(casaEditrice);
+        book.setPrezzo(prezzo);
+        book.setDaAcquistare(daAcquistare);
+        book.setConsigliato(consigliato);
+
+        return bookRepository.save(book);
+    }
+
 }
